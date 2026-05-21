1c76555097d0d0e7619ab312e84b8733f839c159
Vontrelle Collins <vontrellecollins@gmail.com>
2026-05-21 19:48:30 -0400

    styles for top level app and added an embedded google font

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import Counter from "./components/Counter.tsx";
+import "./assets/shared.css";
 
 function App() {
     return (
-        <>
+        <div className="app">
             <Counter 
                 title="Test Counter"
             />
-        </>
+        </div>
     )
 }
 
