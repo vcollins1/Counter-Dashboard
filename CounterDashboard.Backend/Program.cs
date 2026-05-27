3da768699dfa29a9cd457b0615ac3367a383490f
Vontrelle Collins <vontrellecollins@gmail.com>
2026-05-27 08:15:22 -0400

    add cors policy to handle frontend request

--- CounterDashboard.Backend/Program.cs
+++ CounterDashboard.Backend/Program.cs
@@ -1,10 +1,20 @@
 using CounterDashboard.Backend.Data;
 using CounterDashboard.Backend.Endpoints;
 
+const string allowedOrigins = "_allowedOrigins";
 var builder = WebApplication.CreateBuilder(args);
 builder.AddDatabase();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: allowedOrigins, policy =>
+    {
+        policy.WithOrigins("http://localhost:5173").AllowAnyHeader().AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
+app.UseCors(allowedOrigins);
 app.UseCounterEndpoints();
 await app.InitDb();
 
